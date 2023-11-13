9fb1557819deb1760cca369194091f4c0dad7388
conduct13 <phanducan147@gmail.com>
2023-11-13 10:45:54 +0700

    2 player

---
+++ Player 1/src/module/Client.h
@@ -0,0 +1,240 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+#include <ctime>
+using namespace std;
+
+vector<string> History;
+vector<SDL_Rect> destination;
+vector<string> cardImage;
+int Distance = 0;
+int Distance2 = 50;
+int Count = 0;
+int check1 = 0;
+int check2 = 0;
+
+class Client
+{
+
+private:
+
+    ENetHost* client;
+    ENetAddress address;
+    ENetPeer* peer;
+    ENetEvent event;
+
+public:
+    Client() { client = NULL; peer = NULL; }
+
+    unsigned char* getPacketData() { return event.packet->data; }
+
+    int getID() { return 1; } // CLIENT 1
+
+    void setPort(int portID) { address.port = portID; }
+
+    void createHost() { client = enet_host_create(NULL, 1, 1, 0, 0); }
+
+    void setHost(string IP) { enet_address_set_host(&address, IP.c_str()); }
+
+    void setPeer() { peer = enet_host_connect(client, &address, 1, 0); }
+
+    int setHostService(string types, int time);
+
+    void sendCardDataToServer(vector<string> cardSendList);
+
+    void sendDataWhenSkip();
+
+    void Destroy();
+};
+
+int Client::setHostService(string types, int time)
+{
+    if (types.c_str() == "CONNECT")
+    {
+        event.type = ENET_EVENT_TYPE_CONNECT;
+    }
+    else if (types.c_str() == "RECEIVE")
+    {
+        event.type = ENET_EVENT_TYPE_RECEIVE;
+    }
+    else if (types.c_str() == "DISCONNECT")
+    {
+        event.type = ENET_EVENT_TYPE_DISCONNECT;
+    }
+    return enet_host_service(client, &event, time);
+}
+
+void Client::sendCardDataToServer(vector<string> cardSendList)
+{
+    string ID = to_string(getID());
+    for (string& cardPath : cardSendList)
+    {
+        cardPath += ID;
+        string Path = "1" + cardPath;
+        ENetPacket* packet = enet_packet_create(Path.c_str(), Path.length() + 1, ENET_PACKET_FLAG_RELIABLE);
+        enet_peer_send(peer, 0, packet);
+    }
+}
+
+void Client::sendDataWhenSkip()
+{
+    string message = "5aaaaaaaaaaaaaaaaaaaaaaaa5";
+    ENetPacket* packet = enet_packet_create(message.c_str(), message.length() + 1, ENET_PACKET_FLAG_RELIABLE);
+    enet_peer_send(peer, 0, packet);
+}
+
+void Client::Destroy()
+{
+    enet_peer_disconnect(peer, 0);
+
+    while (enet_host_service(client, &event, 3000) > 0)
+    {
+        switch (event.type)
+        {
+        case ENET_EVENT_TYPE_RECEIVE:
+            enet_packet_destroy(event.packet);
+            break;
+        case ENET_EVENT_TYPE_DISCONNECT:
+            cout << "DISCONECT TO SERVER.";
+            break;
+        }
+    }
+}
+
+string getPath(string String)
+{
+    string temp;
+    for (auto it = String.begin() + 1; it != String.end() - 1;it++)
+    {
+
+        temp += *it;
+    }
+    return temp;
+}
+
+void printTurnText(int id)
+{
+    SDL_Texture* turnText = loadTexture("src/image/enemy.png");
+    SDL_Rect destination;
+    destination.w = 150;
+    destination.h = 150;
+    if (id == 1)
+    {
+        destination.x = 20;
+        destination.y = SCREEN_HEIGHT / 2 - 40;
+    }
+    else if (id == 2)
+    {
+        destination.x = SCREEN_WIDTH / 2 - 140;
+        destination.y = SCREEN_HEIGHT / 2 - 240;
+    }
+    else if (id == 3)
+    {
+        destination.x = SCREEN_WIDTH - 240;
+        destination.y = SCREEN_HEIGHT / 2 - 40;
+    }
+
+    SDL_RenderCopy(gRenderer, turnText, NULL, &destination);
+
+}
+
+void printMyTurnText()
+{
+    SDL_Texture* turnText = loadTexture("src/image/turn.png");
+    SDL_Rect destination = { SCREEN_WIDTH / 2 - 135, SCREEN_HEIGHT / 2 + 30, 180, 180 };
+    SDL_RenderCopy(gRenderer, turnText, NULL, &destination);
+
+}
+
+void renderHistoryVer2(vector<string> history) {
+
+    if (history.size() > 0) {
+        for (int i = 0; i < history.size();i++) {
+            SDL_Texture* temp = loadTexture(history[i]);
+            SDL_SetTextureColorMod(temp, 120, 120, 120);
+            SDL_RenderCopy(gRenderer, temp, NULL, &destination[i]);
+        }
+    }
+}
+
+void Reset(SDL_Texture* background, User& player, vector<Computer> computers)
+{
+    SDL_RenderClear(gRenderer);
+    SDL_RenderCopy(gRenderer, backgroundTexture, NULL, NULL);
+    for (Computer computer : computers) {
+        computer.printBackCard();
+    }
+    player.printCards();
+}
+
+void multiplayer(Client client, User& player, vector<Computer>& computers, vector<string>& historyTemp) {
+
+    while (client.setHostService("RECEIVE", 50) > 0) {
+        char buffer[1024] = { '\0' };
+        if (Count >= 3) {
+            history.clear();
+            Reset(backgroundTexture, player, computers);
+            historyTemp.clear();
+            SDL_RenderPresent(gRenderer);
+            Distance = 90;
+            Count = 0;
+        }
+        sprintf_s(buffer, "%s", client.getPacketData());
+        cardImage.push_back(buffer);
+        SDL_Rect temp = { (SCREEN_WIDTH / 4) + 50 + Distance , 250 ,80,115 };
+        destination.push_back(temp);
+        Distance += 90;
+    }
+    for (string& card : cardImage) {
+        string Card = getPath(card);
+        string::iterator it = card.begin();
+        if (*it == '1') {
+            cout << "Receiving information from server.....VALID CARD, ready to print Card : " << Card << " on screen" << endl;
+            historyTemp.push_back(Card);
+            SDL_RenderCopy(gRenderer, loadTexture(Card), NULL, &destination[Count]);
+            printTurnText(2);
+            renderHistory(history);
+            Count++;
+
+        }
+        else if (*it == '2' || *it == '3' || *it == '4') {
+            // Reset(backgroundTexture, player, computers);
+            check1++;
+            if (check1 == 1) {
+                check2++;
+            }
+            if (check2 > 1) {
+                printMyTurnText();
+                check2 = 0;
+            }
+            cout << "Receiving information from server.....VALID CARD, ready to print Card : " << Card << " on screen" << endl;
+            historyTemp.push_back(Card);
+            SDL_RenderCopy(gRenderer, loadTexture(Card), NULL, &destination[Count]);
+           // printTurnText(2);
+            renderHistory(history);
+            Count++;
+
+        }
+        else if (*it == '5') {
+            Reset(backgroundTexture, player, computers);
+            renderHitBtn();
+            renderSkipBtn();
+            renderHistoryVer2(History);
+            check2++;
+            if (check2 > 1) {
+                printMyTurnText();
+                check2 = 0;
+            }
+            printTurnText(2);
+        }
+        SDL_RenderPresent(gRenderer);
+    }
+    check1 = 0;
+    // xoa bo nho dem 
+    cardImage.clear();
+    sendCardList.clear();
+}
+
+
+
---
+++ Player 2/player 2.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+#include<vector>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <cstdio>
+#include<sstream>
+#include<enet/enet.h>
+
+using namespace std;
+
+#include "src/module/global.h"
+#include "src/module/init.h"
+#include "src/module/close.h"
+#include "src/module/texture.h"
+#include "src/module/card.h"
+#include "src/module/user.h"
+#include "src/module/render.h"
+#include "src/module/Client.h"
+#include "src/module/logic_game.h"
+#include "src/module/control.h"
+
+int main(int argc, char* args[])
+{
+
+    if (enet_initialize() != 0) {
+        cout << "ERROR\n";
+        return EXIT_FAILURE;
+    }
+    atexit(enet_deinitialize);
+    Client client;
+    client.createHost();
+    client.setHost("192.168.52.105");
+    client.setPort(8888);
+    client.setPeer();
+
+    // check for connect to server 
+    bool isConnectToServer = false;
+    while (client.setHostService("CONNECT", 8000) > 0) {
+        cout << "CONNECTING TO SERVER SUCCEED!" << endl << "PLEASE WAIT FOR ANOTHER PLAYERS TO PLAY...." << endl;
+        Sleep(4000);
+        isConnectToServer = true;
+        break;
+    }
+
+    // Start up SDL and create window
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else if (!isConnectToServer) {
+        cout << "CAN'T CONNECT TO SERVER";
+    }
+    else
+    {
+        // Main loop flag
+        bool quit = false;
+
+        // Event handler
+        SDL_Event e;
+
+        // set background
+        loadBackground();
+
+        // set a seed
+        srand(time(0));
+
+        // theme of the game
+        themeCard = themesCard[rand() % THEME_NUM];
+
+        // init shuffle deck
+        PlayingCards plCards;
+
+        // init user
+        User player(plCards);
+
+        // init computers
+        vector<Computer> computers;
+        for (int i = 0; i < COMPUTER_NUM; i++)
+        {
+            // init id and 13 cards
+            Computer temp(i + 1, plCards);
+            computers.push_back(temp);
+        }
+        string backPath = "src/cards/" + themeCard + "BACK.png";
+        backTexture = loadTexture(backPath);
+        for (Computer computer : computers)
+            computer.printBackCard();
+
+        hitBtnTexture = loadTexture("src/image/play.png");
+        skipBtnTexture = loadTexture("src/image/skip.png");
+        againBtnTexture = loadTexture("src/image/again.png");
+        printTurnText(2);
+        renderHitBtn();
+        renderSkipBtn();
+        player.printCards();
+        SDL_RenderPresent(gRenderer);
+        // game loop
+        while (!quit)
+        {
+
+            // user play -----------------------------------------------------------------
+            while (SDL_PollEvent(&e) != 0)
+            {
+
+                if (e.type == SDL_QUIT)
+                {
+                    quit = true;
+                }
+                if (e.type == SDL_MOUSEBUTTONUP)    
+                {
+                    int mouseX, mouseY;
+                    SDL_GetMouseState(&mouseX, &mouseY);
+                    // event handler
+                    cardSelectEvent(player, computers, mouseX, mouseY);
+
+                    if (mouseX >= skipBtnArea.x && mouseX <= skipBtnArea.x + skipBtnArea.w &&
+                        mouseY >= skipBtnArea.y && mouseY <= skipBtnArea.y + skipBtnArea.h)
+                    {
+                        client.sendDataWhenSkip();
+                    }
+
+                    if (mouseX >= hitBtnArea.x && mouseX <= hitBtnArea.x + hitBtnArea.w &&
+                        mouseY >= hitBtnArea.y && mouseY <= hitBtnArea.y + hitBtnArea.h)
+                    {
+                        hitBtnEvent(player, computers);
+                        check2++;
+
+                    }
+
+                    SDL_RenderPresent(gRenderer);
+                }
+
+            }
+            client.sendCardDataToServer(sendCardList);
+            multiplayer(client, player, computers, History);
+            renderHistoryVer2(History);
+
+
+            //----------------------------------------------------------------------------                     
+        }
+
+        // Free resources and close SDL
+        close();
+    }
+    client.Destroy();
+    return EXIT_SUCCESS;
+}
